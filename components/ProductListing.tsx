92caff12a68d23934ec4ab697a666b18a8ba95f8
softwareengineer012az <softwareengineer012az@gmail.com>
2025-08-20 01:45:30 +0000

    removing the find my size button form the categories selection

--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -17,11 +17,11 @@ export function ProductListing({ initialProducts }: any) {
 
   return (
     <div className="flex flex-col md:flex-row gap-8">
-      <div className="md:sticky top-24 h-fit">
+      {/* <div className="md:sticky top-24 h-fit">
         <Button asChild className="w-full mt-4">
           <Link href="/fit-quiz">Find My Size</Link>
         </Button>
-      </div>
+      </div> */}
       <div className="flex-1">
         {filteredProducts.length > 0 ? (
           <ProductGrid products={filteredProducts} />
